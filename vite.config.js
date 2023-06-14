88a8b54a77ab59f15e85e22d4a8124a328649128
BlizzardBlast <64456154+BlizzardBlast@users.noreply.github.com>
2023-06-14 09:19:42 +0700

    Idk

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactPlugin from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [
-    react({
-      // Plugin options
+    reactRefresh(),
+    reactPlugin(),
+    react({ plugins: [["@swc/plugin-styled-components", {}]] }),
+    legacy({
+      targets: ["defaults", "not IE 11"],
     }),
   ],
   logLevel: "info",
